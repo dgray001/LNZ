d7d3666e8d2411c741329eec6795b261d1a152fe
dgray001 <dgray@raybeam.com>
2022-03-01 08:14:28 -0600

    v0.6.0b: InitialInterface surface

--- build/source/LNZ.java
+++ build/source/LNZ.java
@@ -21,13 +21,25 @@ public class LNZ extends PApplet {
 
  public void setup() {
   /* size commented out by preprocessor */;
+  surface.setSize(Constants.initialInterfaceSize, Constants.initialInterfaceSize);
+  surface.setLocation(PApplet.parseInt(0.5f * (displayWidth - Constants.initialInterfaceSize)),
+    PApplet.parseInt(0.5f * (displayHeight - Constants.initialInterfaceSize)));
 }
 
  public void draw() {
 }
+static class Constants {
+
+  // Program constants
+  static final String credits =
+  "Liberal Nazi Zombies v0.6.0a" +
+  "Created by Daniel Gray" +
+  "";
+  static final int initialInterfaceSize = 400;
+}
 
 
-  public void settings() { fullScreen(FX2D); }
+  public void settings() { fullScreen(); }
 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "LNZ" };
